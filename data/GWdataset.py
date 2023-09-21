100b10e3f4a55f73e02faba2e799d15ae08bbd4f
Kaze Wong <kazewong.physics@gmail.com>
2023-09-21 17:29:45 -0400

    Black formatting

--- data/GWdataset.py
+++ data/GWdataset.py
@@ -1,9 +1,10 @@
 from kazeML.jax.diffusion.diffusion_dataset import DiffusionDataset
 import h5py
 
+
 class GWdataset(DiffusionDataset):
-    def __init__(self, path, transform=lambda x: x*1e20):
-        self.data = h5py.File(path, 'r')['data']
+    def __init__(self, path, transform=lambda x: x * 1e20):
+        self.data = h5py.File(path, "r")["data"]
         self.transform = transform
 
     def __len__(self):
@@ -13,4 +14,4 @@ class GWdataset(DiffusionDataset):
         sample = self.data[idx]
         if self.transform:
             sample = self.transform(sample)
-        return sample[None,:]
+        return sample[None, :]
