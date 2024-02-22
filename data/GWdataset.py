f9342b2a1e41e5de3b7b0dc38f88ca6f62eefca2
Kaze Wong <kazewong.physics@gmail.com>
2024-02-22 09:49:37 -0500

    Add support for conditional data in GWdataset class

--- data/GWdataset.py
+++ data/GWdataset.py
@@ -3,7 +3,8 @@ import h5py
 
 
 class GWdataset(DiffusionDataset):
-    def __init__(self, path, transform=lambda x: x * 1e20):
+    def __init__(self, path, transform=lambda x: x * 1e20, has_conditional=False):
+        super().__init__(path, transform=transform, has_conditional=has_conditional)
         self.data = h5py.File(path, "r")["data"]
         self.transform = transform
 
@@ -14,4 +15,4 @@ class GWdataset(DiffusionDataset):
         sample = self.data[idx]
         if self.transform:
             sample = self.transform(sample)
-        return sample[None, :]
+        return sample
